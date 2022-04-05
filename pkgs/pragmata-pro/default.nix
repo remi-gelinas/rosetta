89b3e10e7c630dbbee3388f53a620f88280ff3a3
Remi Gelinas <mail@remigelin.as>
2022-04-05 10:51:06 -0400

    feat(pkgs.pragmata-pro): add metadata

--- pkgs/pragmata-pro/default.nix
+++ pkgs/pragmata-pro/default.nix
@@ -3,9 +3,11 @@ let
   version = "0.829";
 
   meta = {
-    description = "";
-    longDescription = '''';
-    homepage = "";
+    description = "PragmataPro™ is a condensed monospaced font optimized for screen.";
+    longDescription = ''
+      PragmataPro™ is a condensed monospaced font optimized for screen, designed by Fabrizio Schiavi to be the ideal font for coding, math and engineering.
+    '';
+    homepage = "https://fsd.it/shop/fonts/pragmatapro";
     license = lib.licenses.unfree;
     platforms = lib.platforms.all;
   };
