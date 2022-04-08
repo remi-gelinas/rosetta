bd200c0515dfc141087c4fdff0156eb7595dffc9
Remi Gelinas <mail@remigelin.as>
2022-04-08 12:45:54 -0400

    refactor(home.fish): swap some aliases to abbreviations

--- home/fish.nix
+++ home/fish.nix
@@ -1,19 +1,25 @@
 { config, lib, pkgs, ... }:
 
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
 
-  programs.fish.shellAliases = with pkgs; {
-    ":q" = "exit";
-    cat = "${bat}/bin/bat";
-    ls = "${exa}/bin/exa";
+    shellAliases = with pkgs; {
+      cat = "${bat}/bin/bat";
+      ls = "${exa}/bin/exa";
+      find = "${fd}/bin/fd";
+    };
+
+    shellAbbrs = {
+      ":q" = "exit";
+    };
+
+    shellInit = ''
+      set -U fish_term24bit 1
+    '';
+
+    interactiveShellInit = ''
+      set -g fish_greeting ""
+    '';
   };
-
-  programs.fish.shellInit = ''
-    set -U fish_term24bit 1
-  '';
-
-  programs.fish.interactiveShellInit = ''
-    set -g fish_greeting ""
-  '';
 }
