8410464ed9b78a23e6b59325ee80f735e4a1f095
Remi Gelinas <mail@remigelin.as>
2023-05-03 12:42:56 -0400

    build: override M1 for GH actions CI

--- configurations/darwin/m1.nix
+++ configurations/darwin/m1.nix
@@ -5,20 +5,32 @@
   inputs,
   ...
 }: let
-  inherit (lib) attrValues makeOverridable;
+  inherit (lib) singleton attrValues makeOverridable;
 in {
-  flake.darwinConfigurations.M1 = withSystem "aarch64-darwin" ({
-    config,
-    pkgs,
-    ...
-  }: let
-    nur-no-pkgs = import inputs.nur {
-      nurpkgs = pkgs;
-      pkgs = throw "nixpkgs eval";
-    };
-  in
-    makeOverridable config.lib.mkDarwinSystem {
-      modules = attrValues self.darwinModules;
-      homeModules = attrValues self.homeManagerModules ++ [nur-no-pkgs.repos.rycee.hmModules.emacs-init];
-    });
+  flake = {
+    darwinConfigurations.M1 = withSystem "aarch64-darwin" ({
+      config,
+      pkgs,
+      ...
+    }: let
+      nur-no-pkgs = import inputs.nur {
+        nurpkgs = pkgs;
+        pkgs = throw "nixpkgs eval";
+      };
+    in
+      makeOverridable config.lib.mkDarwinSystem {
+        modules = attrValues self.darwinModules;
+        homeModules = attrValues self.homeManagerModules ++ [nur-no-pkgs.repos.rycee.hmModules.emacs-init];
+      });
+
+    darwinConfigurations.M1-ci = withSystem "x86_64-darwin" (
+      {system, ...}:
+        self.darwinConfigurations.M1.override {
+          inherit system;
+          username = "runner";
+          nixConfigDirectory = "/Users/runner/work/nixpkgs/nixpkgs";
+          extraModules = singleton {homebrew.enable = lib.mkForce false;};
+        }
+    );
+  };
 }
--- lib/mkDarwinSystem.nix
+++ lib/mkDarwinSystem.nix
@@ -8,66 +8,73 @@
 in {
   perSystem = {
     pkgs,
-    system,
     self',
     ...
   } @ systemLevel: {
-    lib = attrsets.optionalAttrs (builtins.elem system ["x86_64-darwin" "aarch64-darwin"]) {
-      mkDarwinSystem = {
-        modules ? [],
-        extraModules ? [],
-        homeModules ? [],
-        extraHomeModules ? [],
-      }:
-        inputs.darwin.lib.darwinSystem {
-          inherit system;
-          inherit pkgs;
-          inherit inputs;
+    lib = let
+      userCfg = config.remi-nix.primaryUser;
+    in
+      attrsets.optionalAttrs (builtins.elem systemLevel.system ["x86_64-darwin" "aarch64-darwin"]) {
+        mkDarwinSystem = {
+          system ? systemLevel.system,
+          username ? userCfg.username,
+          fullName ? userCfg.fullName,
+          email ? userCfg.email,
+          nixConfigDirectory ? userCfg.nixConfigDirectory,
+          modules ? [],
+          extraModules ? [],
+          homeModules ? [],
+          extraHomeModules ? [],
+        }:
+          inputs.darwin.lib.darwinSystem {
+            inherit system;
+            inherit pkgs;
+            inherit inputs;
 
-          modules =
-            modules
-            ++ extraModules
-            ++ [
-              {
-                config._module.args = {
-                  flakePackages = self'.packages;
-                  flakeConfig = topLevel.config;
-                  systemConfig = systemLevel.config;
-                };
-              }
-            ]
-            ++ [
-              inputs.home-manager.darwinModules.home-manager
-              (_: let
-                user = {
-                  inherit (config.remi-nix.primaryUser) username fullName email nixConfigDirectory;
-                };
-              in {
-                users.primaryUser = user;
-
-                nix.nixPath.nixpkgs = "${inputs.nixpkgs-stable}";
-
-                users.users.${user.username}.home = "/Users/${user.username}";
-
-                home-manager = {
-                  extraSpecialArgs = {
+            modules =
+              modules
+              ++ extraModules
+              ++ [
+                {
+                  config._module.args = {
                     flakePackages = self'.packages;
                     flakeConfig = topLevel.config;
                     systemConfig = systemLevel.config;
                   };
-                  useGlobalPkgs = true;
-                  useUserPackages = true;
-                  users.${user.username} = {
-                    imports = homeModules ++ extraHomeModules;
-                    home = {
-                      stateVersion = config.remi-nix.homeStateVersion;
-                      user-info = user;
+                }
+              ]
+              ++ [
+                inputs.home-manager.darwinModules.home-manager
+                (_: let
+                  user = {
+                    inherit username fullName email nixConfigDirectory;
+                  };
+                in {
+                  users.primaryUser = user;
+
+                  nix.nixPath.nixpkgs = "${inputs.nixpkgs-stable}";
+
+                  users.users.${username}.home = "/Users/${username}";
+
+                  home-manager = {
+                    extraSpecialArgs = {
+                      flakePackages = self'.packages;
+                      flakeConfig = topLevel.config;
+                      systemConfig = systemLevel.config;
+                    };
+                    useGlobalPkgs = true;
+                    useUserPackages = true;
+                    users.${username} = {
+                      imports = homeModules ++ extraHomeModules;
+                      home = {
+                        stateVersion = config.remi-nix.homeStateVersion;
+                        user-info = user;
+                      };
                     };
                   };
-                };
-              })
-            ];
-        };
-    };
+                })
+              ];
+          };
+      };
   };
 }
