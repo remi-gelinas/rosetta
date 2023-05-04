f65d9c2dfe3faf470ccd2b03ff028f7d232efbe7
Remi Gelinas <mail@remigelin.as>
2023-05-04 10:40:53 -0400

    build: add nix-darwin build and switch steps

--- configurations/darwin/m1.nix
+++ configurations/darwin/m1.nix
@@ -5,20 +5,26 @@
   inputs,
   ...
 }: let
-  inherit (lib) attrValues makeOverridable;
+  inherit (lib) singleton mkForce attrValues makeOverridable;
 in {
-  flake.darwinConfigurations.M1 = withSystem "aarch64-darwin" ({
-    config,
-    pkgs,
-    ...
-  }: let
-    nur-no-pkgs = import inputs.nur {
-      nurpkgs = pkgs;
-      pkgs = throw "nixpkgs eval";
+  flake.darwinConfigurations = rec {
+    M1 = withSystem "aarch64-darwin" ({
+      config,
+      pkgs,
+      ...
+    }: let
+      nur-no-pkgs = import inputs.nur {
+        nurpkgs = pkgs;
+        pkgs = throw "nixpkgs eval";
+      };
+    in
+      makeOverridable config.lib.mkDarwinSystem {
+        modules = attrValues self.darwinModules;
+        homeModules = attrValues self.homeManagerModules ++ [nur-no-pkgs.repos.rycee.hmModules.emacs-init];
+      });
+
+    M1-ci = M1.override {
+      extraModules = singleton {homebrew.enable = mkForce false;};
     };
-  in
-    makeOverridable config.lib.mkDarwinSystem {
-      modules = attrValues self.darwinModules;
-      homeModules = attrValues self.homeManagerModules ++ [nur-no-pkgs.repos.rycee.hmModules.emacs-init];
-    });
+  };
 }
