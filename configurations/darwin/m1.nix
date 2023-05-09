662d28c31f4688158275bf93b25d488b5f32093f
Remi Gelinas <mail@remigelin.as>
2023-05-09 19:44:13 -0400

    refactor: hoist system creation

--- configurations/darwin/m1.nix
+++ configurations/darwin/m1.nix
@@ -3,79 +3,69 @@
   inputs,
   ...
 }: let
-  fetchSystemConfig = system: let
+  mkSystem = {
+    system,
+    primaryUser,
+    modules ? [],
+    extraModules ? [],
+    homeModules ? [],
+    extraHomeModules ? [],
+  }: let
     pkgs = import inputs.nixpkgs-stable {inherit system;};
+    nur = import inputs.nur {
+      nurpkgs = pkgs;
+      pkgs = throw "nixpkgs eval";
+    };
   in {
-    inherit
-      (
-        (import inputs.nur {
-          nurpkgs = pkgs;
-          pkgs = throw "nixpkgs eval";
-        })
-        .repos
-        .rycee
-        .hmModules
-      )
-      emacs-init
-      ;
+    inherit system;
 
-    primaryUser.gpgKey = rec {
-      master = "3393D1E11D5CA44F06A809DB8661D12F66E5070C";
+    primaryUser =
+      primaryUser
+      // {
+        gpgKey = rec {
+          master = "3393D1E11D5CA44F06A809DB8661D12F66E5070C";
 
-      subkeys = {
-        authentication = "9DCF2C0022213A78B20A8CCE61E8ED0C55C0FE3E";
-        encryption = "A799BCF5EC76C2626E831B8E412FD0A804B6 C182";
-        signing = "E029B3CBB21F86F30842BC80F953929D57ECBAC0";
+          subkeys = {
+            authentication = "9DCF2C0022213A78B20A8CCE61E8ED0C55C0FE3E";
+            encryption = "A799BCF5EC76C2626E831B8E412FD0A804B6 C182";
+            signing = "E029B3CBB21F86F30842BC80F953929D57ECBAC0";
+          };
+
+          publicKey = let
+            keyFile = pkgs.fetchurl {
+              url = "https://keys.openpgp.org/vks/v1/by-fingerprint/${master}";
+              sha256 = "sha256-wy6Cqo7cJ2J4+FVJTARH/a5xzLedKlHZej35+8aoHf8=";
+            };
+          in
+            builtins.readFile keyFile;
+        };
       };
 
-      publicKey = let
-        keyFile = pkgs.fetchurl {
-          url = "https://keys.openpgp.org/vks/v1/by-fingerprint/${master}";
-          sha256 = "sha256-wy6Cqo7cJ2J4+FVJTARH/a5xzLedKlHZej35+8aoHf8=";
-        };
-      in
-        builtins.readFile keyFile;
-    };
+    homeModules = [nur.repos.rycee.hmModules.emacs-init] ++ homeModules ++ extraHomeModules;
+    modules = modules ++ extraModules;
   };
-in rec {
-  config.remi-nix.darwinConfigurations.M1 = let
+in {
+  config.remi-nix.darwinConfigurations.M1 = mkSystem {
     system = "aarch64-darwin";
-    systemArgs = fetchSystemConfig system;
-  in rec {
-    inherit system;
 
     primaryUser = rec {
       fullName = "Remi Gelinas";
       username = "rgelinas";
       email = "mail@remigelin.as";
       nixConfigDirectory = "/Users/${username}/.config/nixpkgs";
-
-      inherit (systemArgs.primaryUser) gpgKey;
     };
-
-    homeModules = [
-      systemArgs.emacs-init
-    ];
   };
 
-  config.remi-nix.darwinConfigurations.M1-ci = let
+  config.remi-nix.darwinConfigurations.M1-ci = mkSystem {
     system = "x86_64-darwin";
-    systemArgs = fetchSystemConfig system;
-  in
-    config.remi-nix.darwinConfigurations.M1
-    // rec {
-      inherit system;
 
-      primaryUser = {
-        username = "runner";
-        fullName = "";
-        email = "";
-        nixConfigDirectory = "/Users/runner/work/nixpkgs/nixpkgs";
-
-        inherit (systemArgs.primaryUser) gpgKey;
-      };
-
-      homeModules = [systemArgs.emacs-init];
-      modules = [{homebrew.enable = lib.mkForce false;}];
+    primaryUser = {
+      username = "runner";
+      fullName = "";
+      email = "";
+      nixConfigDirectory = "/Users/runner/work/nixpkgs/nixpkgs";
     };
+
+    modules = [{homebrew.enable = lib.mkForce false;}];
+  };
 }
