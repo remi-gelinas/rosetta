e65c1dcebff8172a9a54f789015bc581a0e674df
Remi Gelinas <mail@remigelin.as>
2023-06-07 15:45:46 -0400

    feat(parts:emacs): allow extra binaries to be added to the exec-path

--- parts/emacs/default.nix
+++ parts/emacs/default.nix
@@ -35,6 +35,16 @@ localFlake: {
         type = types.package;
         readOnly = true;
       };
+
+      binaries = mkOption {
+        type = types.listOf types.package;
+        default = [];
+      };
+
+      extraBinaries = mkOption {
+        type = types.listOf types.package;
+        default = [];
+      };
     };
 
     config.emacs = let
@@ -52,10 +62,28 @@ localFlake: {
       init = builtins.readFile "${emacs-config-org}/init.el" + cfg.extraInit;
       earlyInit = builtins.readFile "${emacs-config-org}/early-init.el" + cfg.extraEarlyInit;
 
-      package = pkgs.callPackage (import config.legacyPackages.editors.emacs {
-        inherit (localFlake.inputs) emacs-unstable;
-        config = cfg.init;
-      }) {};
+      package = let
+        directories = cfg.binaries ++ cfg.extraBinaries;
+
+        appendExtraBinaryDirectories = lib.optionalString ((builtins.length directories) != 0) ''
+          (setq exec-path (append exec-path '( ${
+            lib.concatMapStringsSep " " (x: ''"${x}/bin"'') directories
+          } )))
+          (setenv "PATH" (concat (getenv "PATH") ":${
+            lib.concatMapStringsSep ":" (x: "${x}/bin") directories
+          }"))
+        '';
+      in
+        pkgs.callPackage (import config.legacyPackages.editors.emacs {
+          inherit (localFlake.inputs) emacs-unstable;
+          config = cfg.init + appendExtraBinaryDirectories;
+        }) {};
+
+      # Packages that are available to the emacs package system-wide.
+      binaries = with pkgs; [
+        direnv
+        python311
+      ];
     };
 
     # TODO: Enable check when https://github.com/NixOS/nix/pull/7759 is included in a CI-installable Nix version
