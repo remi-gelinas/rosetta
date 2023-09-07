fbc83a2fc3fb4503c9df21d99b80a192e30d41b5
Remi Gelinas <mail@remigelin.as>
2023-09-07 12:59:32 -0400

    feat(pre-commit-hooks): NO-ISSUE disable pre-commit checks

--- parts/pre-commit-hooks.nix
+++ parts/pre-commit-hooks.nix
@@ -1,13 +1,17 @@
 _: _: {
   perSystem = _: {
-    pre-commit.settings = {
-      settings.deadnix.exclude = ["./_sources/generated.nix"];
+    pre-commit = {
+      check.enable = false;
 
-      hooks = {
-        alejandra.enable = true;
-        deadnix.enable = true;
-        statix.enable = true;
-        commitizen.enable = true;
+      settings = {
+        settings.deadnix.exclude = ["./_sources/generated.nix"];
+
+        hooks = {
+          alejandra.enable = true;
+          deadnix.enable = true;
+          statix.enable = true;
+          commitizen.enable = true;
+        };
       };
     };
   };
