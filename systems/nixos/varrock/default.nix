123cf07923300d460085c747c7035cb1938d381c
Remi Gelinas <mail@remigelin.as>
2023-09-15 18:40:09 -0400

    feat(systems:nixos:varrock): NO-ISSUE add stuff

--- systems/nixos/varrock/default.nix
+++ systems/nixos/varrock/default.nix
@@ -11,9 +11,17 @@
       hyprland.homeManagerModules.default
       ({pkgs, ...}: {
         inherit (config) nixpkgsConfig colors;
-        wayland.windowManager.hyprland.enable = true;
+        wayland.windowManager.hyprland = {
+          enable = true;
+          package = null;
 
-        packages = [
+          extraConfig = ''
+            exec-once=${pkgs.dunst}/bin/dunst
+            bindr=SUPER, SPACE, exec, pkill ${pkgs.tofi}/bin/tofi || ${pkgs.tofi}/bin/tofi
+          '';
+        };
+
+        home.packages = [
           nixpkgs-unstable.legacyPackages.${system}.asusctl
           pkgs.lutris
         ];
@@ -41,7 +49,16 @@
         };
 
         environment.sessionVariables.NIXOS_OZONE_WL = "1";
-        environment.systemPackages = [pkgs.git pkgs.kitty];
+        environment.systemPackages = with pkgs; [
+          git
+          kitty
+        ];
+
+        services.pipewire = {
+          enable = true;
+          audio.enable = true;
+          wireplumber.enable = true;
+        };
       })
       {
         services.asusd = {
