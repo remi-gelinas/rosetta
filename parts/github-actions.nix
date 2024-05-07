f3e88c2627da5eb653fa311232fb563e378138d7
Remi Gelinas <mail@remigelin.as>
2024-05-06 20:42:54 -0400

    revert: use normal Apple Silicon runners

--- parts/github-actions.nix
+++ parts/github-actions.nix
@@ -5,7 +5,7 @@ let
   # Architecture to Github Runner label mappings
   platforms = {
     x86_64-linux = "ubuntu-latest";
-    aarch64-darwin = "macos-14-xlarge";
+    aarch64-darwin = "macos-14";
   };
 in
 {
