f212366e628de46477a0149c0f07f6170e86c0ae
Remi Gelinas <mail@remigelin.as>
2024-05-16 13:23:11 -0400

    refactor(parts:darwin-configurations): make systems config unique

--- parts/darwin-configurations.nix
+++ parts/darwin-configurations.nix
@@ -15,112 +15,114 @@ in
   _file = ./darwin-configurations.nix;
 
   options.darwinConfigurations = mkOption {
-    type = types.attrsOf (
-      types.submodule (
-        { config, ... }:
-        {
-          options = {
-            system = mkOption {
-              type = types.enum [
-                "aarch64-darwin"
-                "x86_64-darwin"
-              ];
-            };
+    type = types.submodule {
+      freeformType = types.attrsOf (
+        types.submodule (
+          { config, ... }:
+          {
+            options = {
+              system = mkOption {
+                type = types.enum [
+                  "aarch64-darwin"
+                  "x86_64-darwin"
+                ];
+              };
 
-            homeStateVersion = mkOption {
-              type = types.str;
-              default = "23.05";
-            };
+              homeStateVersion = mkOption {
+                type = types.str;
+                default = "23.05";
+              };
 
-            primaryUser = {
-              username = mkOption { type = types.str; };
+              primaryUser = {
+                username = mkOption { type = types.str; };
 
-              fullName = mkOption { type = types.str; };
+                fullName = mkOption { type = types.str; };
 
-              email = mkOption { type = types.str; };
+                email = mkOption { type = types.str; };
 
-              nixConfigDirectory = mkOption { type = types.str; };
+                nixConfigDirectory = mkOption { type = types.str; };
 
-              gpgKey = {
-                master = mkOption { type = types.str; };
+                gpgKey = {
+                  master = mkOption { type = types.str; };
 
-                publicKey = mkOption { type = types.str; };
+                  publicKey = mkOption { type = types.str; };
 
-                subkeys = {
-                  authentication = mkOption { type = types.str; };
+                  subkeys = {
+                    authentication = mkOption { type = types.str; };
 
-                  encryption = mkOption { type = types.str; };
+                    encryption = mkOption { type = types.str; };
 
-                  signing = mkOption { type = types.str; };
+                    signing = mkOption { type = types.str; };
+                  };
                 };
+              };
+
+              modules = mkOption {
+                type = types.listOf types.unspecified;
+                default = [ ];
+              };
+
+              homeModules = mkOption {
+                type = types.listOf types.unspecified;
+                default = [ ];
+              };
+
+              finalModules = lib.mkOption {
+                type = lib.types.listOf lib.types.unspecified;
+                readOnly = true;
+              };
+
+              finalSystem = lib.mkOption {
+                type = lib.types.unspecified;
+                readOnly = true;
               };
             };
 
-            modules = mkOption {
-              type = types.listOf types.unspecified;
-              default = [ ];
-            };
-
-            homeModules = mkOption {
-              type = types.listOf types.unspecified;
-              default = [ ];
-            };
-
-            finalModules = lib.mkOption {
-              type = lib.types.listOf lib.types.unspecified;
-              readOnly = true;
-            };
-
-            finalSystem = lib.mkOption {
-              type = lib.types.unspecified;
-              readOnly = true;
-            };
-          };
-
-          config = {
-            finalModules = [
-              inputs.home-manager.darwinModules.home-manager
-              {
-                home-manager = {
-                  sharedModules = config.homeModules;
-                };
-              }
-              (
-                _:
-                let
-                  user = config.primaryUser;
-                in
+            config = {
+              finalModules = [
+                inputs.home-manager.darwinModules.home-manager
                 {
-                  users.primaryUser = user;
-                  users.users.${user.username}.home = "/Users/${user.username}";
                   home-manager = {
-                    useGlobalPkgs = true;
-                    useUserPackages = true;
-                    users.${user.username} = {
-                      home = {
-                        stateVersion = config.homeStateVersion;
-                        user-info = user;
-                      };
-                    };
+                    sharedModules = config.homeModules;
                   };
                 }
-              )
-            ] ++ config.modules;
+                (
+                  _:
+                  let
+                    user = config.primaryUser;
+                  in
+                  {
+                    users.primaryUser = user;
+                    users.users.${user.username}.home = "/Users/${user.username}";
+                    home-manager = {
+                      useGlobalPkgs = true;
+                      useUserPackages = true;
+                      users.${user.username} = {
+                        home = {
+                          stateVersion = config.homeStateVersion;
+                          user-info = user;
+                        };
+                      };
+                    };
+                  }
+                )
+              ] ++ config.modules;
 
-            finalSystem = inputs.darwin.lib.darwinSystem {
-              inherit (config) system;
+              finalSystem = inputs.darwin.lib.darwinSystem {
+                inherit (config) system;
 
-              modules = config.finalModules;
+                modules = config.finalModules;
+              };
             };
-          };
-        }
-      )
-    );
+          }
+        )
+      );
+    };
   };
 
   options.flake.darwinConfigurations = mkOption { type = types.lazyAttrsOf types.unspecified; };
 
-  config.darwinConfigurations = (import ../systems args).darwin;
+  config.darwinConfigurations = lib.mkDefault (import ../systems args).darwin;
 
   config.flake = {
     darwinConfigurations = systems;
