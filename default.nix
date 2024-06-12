aa28bfb0004552fd2568e683c5fb72eaaa81b858
Remi Gelinas <mail@remigelin.as>
2024-06-11 22:47:39 -0400

    chore: remove redundant flake-compat

--- default.nix
+++
@@ -1,9 +0,0 @@
-(import (
-  let
-    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
-  in
-  fetchTarball {
-    inherit (lock.nodes.flake-compat.locked) url;
-    sha256 = lock.nodes.flake-compat.locked.narHash;
-  }
-) { src = ./.; }).defaultNix
