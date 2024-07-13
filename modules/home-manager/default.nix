cd3d5d0c0866200fc328ff2934654cd40f1fa44c
Remi Gelinas <mail@remigelin.as>
2024-07-13 19:19:29 -0400

    feat: add SSH config for 1Password socket

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -9,6 +9,7 @@ rosetta: {
   gpg = ./gpg.nix;
   packages = ./packages.nix;
   rosetta-bridge = import ./rosetta-bridge.nix rosetta;
+  ssh = ./ssh.nix;
   starship = ./starship.nix;
   thefuck = ./thefuck.nix;
   trampolines = ./trampolines.nix;
