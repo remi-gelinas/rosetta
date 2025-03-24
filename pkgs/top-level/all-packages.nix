a89b5df1f69d51e0df751923ed229e154d0bd3aa
Remi Gelinas <mail@remigelin.as>
2025-03-24 08:21:01 -0400

    chore: clean redundant packages

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2,8 +2,5 @@
 with pkgs;
 {
   aerospace = callPackage ../by-name/ae/aerospace/package.nix { };
-  foundry = callPackage ../by-name/fo/foundry/package.nix { };
   gh-poi = callPackage ../by-name/gh/gh-poi/package.nix { };
-  safecard-cli = callPackage ../by-name/sa/safecard-cli/package.nix { };
-  soldeer = callPackage ../by-name/so/soldeer/package.nix { };
 }
